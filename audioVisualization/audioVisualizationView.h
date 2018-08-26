7a6e3ae94d1563ffb6f7c0d3d61b79028a8b2a4d
Champ Kongsil <info.bughacking@gmail.com>
2018-08-26 09:04:20 +0700

    Initial Commit

---
+++ audioVisualization/audioVisualizationView.h
@@ -0,0 +1,13 @@
+//
+//  audioVisualizationView.h
+//  audioVisualization
+//
+//  Created by Champ on 26/8/2561 BE.
+//  Copyright © 2561 Champ. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface audioVisualizationView : ScreenSaverView
+
+@end
---
+++ audioVisualization/audioVisualizationView.m
@@ -0,0 +1,52 @@
+//
+//  audioVisualizationView.m
+//  audioVisualization
+//
+//  Created by Champ on 26/8/2561 BE.
+//  Copyright © 2561 Champ. All rights reserved.
+//
+
+#import "audioVisualizationView.h"
+
+@implementation audioVisualizationView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
